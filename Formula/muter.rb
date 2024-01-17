2a19c874aabad7f56a6f73febbd7171a605d8feb
EduuardP <62478411+EduuardP@users.noreply.github.com>
2024-01-16 19:30:58 -0500

    Update muter.rb

--- Formula/muter.rb
+++ Formula/muter.rb
@@ -1,7 +1,7 @@
 class Muter < Formula
 	desc "Automated mutation testing for Swift"
 	homepage "https://github.com/muter-mutation-testing/muter/"
-	url "https://github.com/muter-mutation-testing/muter/archive/refs/tags/15.zip"
+	url "https://github.com/muter-mutation-testing/muter/archive/refs/tags/v15.zip"
 	sha256 "de8c5f73f5f2810e83e6c72a5a429d0328f90df25842f8c581b4e66a3c694076"
 	
 	def install
